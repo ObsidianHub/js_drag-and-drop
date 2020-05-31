1cd312972a421b6f5a0783143b3afd4e2d549225
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-05-31 23:03:01 +0300

    start creating collide method inside ConvexChape2D class -> create main vars

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ var o = document.querySelector("p");
 
 class ConvexShape2D {
   constructor() {}
+
+  collide(shape, d) {
+    var a = this.getExtremePoint(d[0], d[1]); // get the extreme point on this shape
+    var b = shape.getExtremePoint(-d[0], -d[1]); // get the opposite extreme point on the other shape
+    var p = [a[0] - b[0], a[1] - b[1]]; // get the extreme point on the Minkowski Difference shape
+  }
 }
