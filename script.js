ea6d169c4aaef731ac06e1eeaf6a36f2845f5759
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-05-31 23:04:30 +0300

    start writing while loop

--- script.js
+++ script.js
@@ -14,5 +14,15 @@ class ConvexShape2D {
     var s = [[p[0], p[1]]]; // the simplex now has one point in it
 
     d = [-p[0], -p[1]]; // search in the direction of the origin next
+
+    while (true) {
+      a = this.getExtremePoint(d[0], d[1]); // get the extreme point on this shape
+      b = shape.getExtremePoint(-d[0], -d[1]); // get the opposite extreme point on the other shape
+      p = [a[0] - b[0], a[1] - b[1]]; // get the extreme point on the Minkowski Difference shape
+
+      if (p[0] * d[0] + p[1] * d[1] < 0) return false; // if the origin is beyond p in direction d, return false
+
+      let tc = Boolean(s[0][0] * p[1] - s[0][1] * p[0] < 0); // the test cross product will be + / - depending on winding; It doesn't matter what the winding is so long as it is the same for every line segment in the simplex.
+    }
   }
 }
