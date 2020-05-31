e3d29410af6688fcb260a4cbbe3d4a07060c12fa
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-05-31 23:03:29 +0300

    add first if construction with description

--- script.js
+++ script.js
@@ -7,5 +7,7 @@ class ConvexShape2D {
     var a = this.getExtremePoint(d[0], d[1]); // get the extreme point on this shape
     var b = shape.getExtremePoint(-d[0], -d[1]); // get the opposite extreme point on the other shape
     var p = [a[0] - b[0], a[1] - b[1]]; // get the extreme point on the Minkowski Difference shape
+
+    if (p[0] * d[0] + p[1] * d[1] < 0) return false; // if the origin is beyond p in direction d, return false
   }
 }
