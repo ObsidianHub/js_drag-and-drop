054c987d3c1778e9417c448ab00a7df190132d1d
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-02-07 23:53:05 +0200

    update DOM after adding item from input

--- trello-clone-d&d/script.js
+++ trello-clone-d&d/script.js
@@ -108,6 +108,15 @@ function updateDOM() {
   updateSavedColumns();
 }
 
+// Add to column list, reset text box
+function addToColumn(column) {
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = "";
+  updateDOM();
+}
+
 // Show add item input box
 function showInputBox(column) {
   addBtns[column].style.visibility = "hidden";
@@ -120,6 +129,7 @@ function hideInputBox(column) {
   addBtns[column].style.visibility = "visible";
   saveItemBtns[column].style.display = "none";
   addItemContainers[column].style.display = "none";
+  addToColumn(column);
 }
 
 // Allows arrays to reflect DnD items
